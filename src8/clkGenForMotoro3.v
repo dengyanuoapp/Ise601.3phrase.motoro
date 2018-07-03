379f76e6159b923ae0ad80a2a245a80e7c47c95b
Your Name <you@example.com>
2018-07-03 14:23:26 +0800

    _2018_0703__1423pm

--- src8/clkGenForMotoro3.v
+++ src8/clkGenForMotoro3.v
@@ -9,20 +9,23 @@ output  reg                 clkM3 ;
 input   wire                nRst;		
 input   wire                clk50mhz;
 
-reg     [2:0]               cnt ;	
+reg     [3:0]               cnt ;	
 
 always @ (negedge clk50mhz or negedge nRst) begin
     if(!nRst) begin
+        cnt             <= 4'd4 ;
         clkM3           <= 1'b0 ;
-        cnt             <= 3'd4 ;
     end
     else begin
-        if ( cnt == 3'd0 ) begin
-            clkM3       <= ~clkM3 ;
-            cnt         <= 3'd4 ;
+        if ( cnt == 4'd0 ) begin
+            cnt         <= 4'd4 ;
+            clkM3       <= 1'b0 ;
         end 
         else begin
-            cnt         <= cnt - 3'd1 ;
+            if ( cnt == 4'd2 ) begin
+                clkM3   <= 1'b1 ;
+            end 
+            cnt         <= cnt - 4'd1 ;
         end
     end
 end
