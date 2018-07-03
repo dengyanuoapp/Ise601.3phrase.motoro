94802510d62d7d8da03af30c912ad6c2642e8324
Your Name <you@example.com>
2018-07-03 19:21:51 +0800

    _2018_0703__1921pm

--- src8/motoro3_top.v
+++ src8/motoro3_top.v
@@ -16,12 +16,12 @@ module motoro3_top(
 
 );
 
-output  wire                aH ;	
-output  wire                aL ;	
-output  wire                bH ;	
-output  wire                bL ;	
-output  wire                cH ;	
-output  wire                cL ;	
+output  reg                 aH ;	
+output  reg                 aL ;	
+output  reg                 bH ;	
+output  reg                 bL ;	
+output  reg                 cH ;	
+output  reg                 cL ;	
 
 input   wire                m3start;	
 input   wire                m3invOrStop;	 
@@ -38,6 +38,13 @@ reg             [9:0]       m3freq_clked1        ;
 //reg                         m3start_clked2       ;	
 //reg                         m3invOrStop_clked2   ;	 
 //reg             [9:0]       m3freq_clked2        ;	
+
+wire                        aH_ii       ;	
+wire                        aL_ii       ;	
+wire                        bH_ii       ;	
+wire                        bL_ii       ;	
+wire                        cH_ii       ;	
+wire                        cL_ii       ;	
 
 always @ (posedge clk or negedge nRst) begin
     if(!nRst) begin
@@ -57,6 +64,26 @@ always @ (posedge clk or negedge nRst) begin
     end
 end
 
+always @ (posedge clk or negedge nRst) begin
+    if(!nRst) begin
+        aH                          <= 0                ;
+        aL                          <= 0                ;
+        bH                          <= 0                ;
+        bL                          <= 0                ;
+        cH                          <= 0                ;
+        cL                          <= 0                ;
+    end
+    else begin
+        aH                          <= aH_ii            ;
+        aL                          <= aL_ii            ;
+        bH                          <= bH_ii            ;
+        bL                          <= bL_ii            ;
+        cH                          <= cH_ii            ;
+        cL                          <= cL_ii            ;
+    end
+end
+
+
 //always @ (negedge clk or negedge nRst) begin
 //    if(!nRst) begin
 //        m3start_clked2              <= 0                   ;
@@ -73,12 +100,12 @@ end
 motoro3_real
 r
 (
-    .aH                     (   aH                      ),
-    .aL                     (   aL                      ),
-    .bH                     (   bH                      ),
-    .bL                     (   bL                      ),
-    .cH                     (   cH                      ),
-    .cL                     (   cL                      ),
+    .aH                     (   aH_ii                   ),
+    .aL                     (   aL_ii                   ),
+    .bH                     (   bH_ii                   ),
+    .bL                     (   bL_ii                   ),
+    .cH                     (   cH_ii                   ),
+    .cL                     (   cL_ii                   ),
                                                
 //    .m3start                (   m3start_clked2           ),
 //    .m3freq                 (   m3freq_clked2            ),
