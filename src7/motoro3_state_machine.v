7da40d43777a84119c819201396da2e8472c5c01
Your Name <you@example.com>
2018-07-03 10:29:10 +0800

    _2018_0703__1029am

--- src7/motoro3_state_machine.v
+++ src7/motoro3_state_machine.v
@@ -24,8 +24,10 @@ input   wire    [9:0]       m3freq;
 input   wire                clk;			// 10MHz
 input   wire                nRst;		
 
+wire            [16:0]      m3cnt_reload1;	
 reg                         m3start_clked1;	
 wire                        m3start_up1;	
+wire                        m3cntLast1 = m3cnt[16] ;
 
 assign  m3start_up1 = (m3start) && (~m3start_clked1) ;
 always @ (negedge clk or negedge nRst) begin
@@ -38,14 +40,15 @@ always @ (negedge clk or negedge nRst) begin
 end
 
 
+assign m3cnt_reload1 = { 1'd0, m3freq , 6'd0 };
 always @ (negedge clk or negedge nRst) begin
     if(!nRst) begin
-        m3cnt               <= { 1'd0, m3freq , 6'd0 };
+        m3cnt               <= m3cnt_reload1            ;
         m3cnt               <= 17'd32;
     end
     else begin
-        if ( m3start_up1 == 1 ) begin
-            m3cnt           <= { 1'd0, m3freq , 6'd0 };
+        if ( m3start_up1 == 1 || m3cntLast1 == 1) begin
+            m3cnt           <= m3cnt_reload1            ;
             m3cnt           <= 17'd32;
         end
         else begin
@@ -65,7 +68,7 @@ always @ (negedge clk or negedge nRst) begin
             m3step              <= 4'd1                 ;
         end
         else begin
-            if ( m3cnt == 0 ) begin
+            if ( m3cntLast1 == 1'd1 ) begin
                 if ( m3step == 4'd6 ) begin
                     m3step      <= 4'd1                 ;
                 end
