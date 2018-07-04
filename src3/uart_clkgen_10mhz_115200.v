5e50542c9861f25aea06ec112216e8e3d1dc1552
Your Name <you@example.com>
2018-07-04 17:00:30 +0800

    _2018_0704__1700pm

--- src3/uart_clkgen_10mhz_115200.v
+++ src3/uart_clkgen_10mhz_115200.v
@@ -1,42 +1,56 @@
-module uart_set_show_config_top(
-    uTx,
-    clkUtx ,
 
-    nrst,
+module uart_clkgen_10mhz_115200(
+    bps_start,
+    clkUtx,
+
+    rst_n,
     clk10mhz
-
 );
 
-input   wire                clk10mhz;			// 50MHz
-input   wire                nrst;		// reset button on the core board
-                           
-output  wire                clkUtx;	
-output  wire                uTx;	
+input   wire                clk10mhz;	// 50MHz
+input   wire                rst_n;	//
+input   wire                bps_start;	//
+output  wire                clkUtx;	// clkUtx 
 
+/*
+parameter 		bps9600 	= 5207,	//9600bps
+bps19200 	= 2603,	//19200bps
+bps38400 	= 1301,	//38400bps
+bps57600 	= 867,	//57600bps
+bps115200	= 433;	//115200bps
 
-wire bps_start2;	
-wire[7:0] rx_data;	
-wire rx_int;		
+parameter 		bps9600_2 	= 2603,
+bps19200_2	= 1301,
+bps38400_2	= 650,
+bps57600_2	= 433,
+bps115200_2 = 216;  
+*/
 
-assign rx_data = 'h08;
+//
+`define		BPS_PARA		5207	//9600
+`define 	BPS_PARA_2		2603	//9600
 
-uart_clkgen_10mhz_115200		
-ucg01(	
-    .clk10mhz(clk10mhz),	//
-    .rst_n(nrst),
-    .bps_start(bps_start2),
-    .clkUtx(clkUtx)
-);
+reg[12:0] cnt;			//
+reg clk_tx_reg;			//
 
-uart_tx			
-tx01(		
-    .clk10mhz(clk10mhz),	//
-    .rst_n(nrst),
-    .rx_data(rx_data),
-    .rx_int(rx_int),
-    .uTx(uTx),
-    .clkUtx(clkUtx),
-    .bps_start(bps_start2)
-);
+//----------------------------------------------------------
+reg[2:0] uart_ctrl;	// uart
+//----------------------------------------------------------
+
+always @ (posedge clk10mhz or negedge rst_n) begin
+    if(!rst_n) cnt <= 13'd0;
+    else if((cnt == `BPS_PARA) || !bps_start) cnt <= 13'd0;	//
+    else cnt <= cnt+1'b1;			//
+end
+
+always @ (posedge clk10mhz or negedge rst_n) begin
+    if(!rst_n) clk_tx_reg <= 1'b0;
+    //&& bps_start
+    else if(cnt == `BPS_PARA_2 && bps_start) clk_tx_reg <= 1'b1;	// clk_tx_reg,
+    else clk_tx_reg <= 1'b0;
+
+end
+
+assign clkUtx = clk_tx_reg;
 
 endmodule
---
+++ src3/uart_set_show_config_top.v
@@ -0,0 +1,42 @@
+module uart_set_show_config_top(
+    uTx,
+    clkUtx ,
+
+    nrst,
+    clk10mhz
+
+);
+
+input   wire                clk10mhz;			// 50MHz
+input   wire                nrst;		// reset button on the core board
+                           
+output  wire                clkUtx;	
+output  wire                uTx;	
+
+
+wire bps_start2;	
+wire[7:0] rx_data;	
+wire rx_int;		
+
+assign rx_data = 'h08;
+
+uart_clkgen_10mhz_115200		
+ucg01(	
+    .clk10mhz(clk10mhz),	//
+    .rst_n(nrst),
+    .bps_start(bps_start2),
+    .clkUtx(clkUtx)
+);
+
+uart_tx			
+tx01(		
+    .clk10mhz(clk10mhz),	//
+    .rst_n(nrst),
+    .rx_data(rx_data),
+    .rx_int(rx_int),
+    .uTx(uTx),
+    .clkUtx(clkUtx),
+    .bps_start(bps_start2)
+);
+
+endmodule
