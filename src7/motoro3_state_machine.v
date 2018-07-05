74c6ade2bdcaa84dadd4d3026c12bd0e8857bca1
Your Name <you@example.com>
2018-07-05 11:30:42 +0800

    _2018_0705__1130am : add XSTdefine

--- src7/motoro3_state_machine.v
+++ src7/motoro3_state_machine.v
@@ -86,14 +86,29 @@ always @( m3step ) begin
     endcase
 end
 
+`ifndef m3cnt_reload1_now 
+`ifdef synthesising
+`define m3cnt_reload1_now    25'd666_666
+`endif
+`ifdef simulatingVERIDI
+`define m3cnt_reload1_now    25'd16_667
+`endif
+`endif
+
+`ifndef m3cnt_reload1_now 
+initial begin
+$error( "you should define synthesising/simulatingVERIDI , or m3cnt_reload1_now, then run again" );
+end
+`endif
 
 //assign m3cnt_reload1 = { 1'd0, m3freq , 6'd0 };
 //assign m3cnt_reload1 = 25'd1_667      ; // 6*1_667        == 1,000.2 us       == 1000Hz
 //assign m3cnt_reload1 = 25'd16_667     ; // 6*16_667       == 10,000.2 us      == 100Hz
 //assign m3cnt_reload1 = 25'd166_667    ; // 6*166_667      == 100,000.2 us     == 10Hz
 //assign m3cnt_reload1 = 25'd333_333    ; // 6*333_333      == 200,000.4 us     == 5Hz
-assign m3cnt_reload1 = 25'd666_666    ; // 6*666_666      == 400,000.8 us     == 2.5Hz
+//assign m3cnt_reload1 = 25'd666_666    ; // 6*666_666      == 400,000.8 us     == 2.5Hz
 //assign m3cnt_reload1 = 25'd1_666_667  ; // 6*1_666_667    == 1,000,000.2 us   == 1Hz
+assign m3cnt_reload1 = `m3cnt_reload1_now ;
 always @ (negedge clk or negedge nRst) begin
     if(!nRst) begin
         m3cnt               <= m3cnt_reload1            ;
