48c4152bce9004358182a45a3fdf203d30d0e5a1
Your Name <you@example.com>
2018-07-20 09:28:22 +0800

    _2018_0720__0928am

--- src5/motoro3_calc_pwm_len.v
+++ src5/motoro3_calc_pwm_len.v
@@ -11,12 +11,12 @@ module motoro3_calc_pwm_len(
 
 input   wire    [7:0]       m3r_power_percent       ;	// to control the percent of power , max 255 % , min 1 %.
 input   wire    [24:0]      m3r_stepCNT_speedSET    ;	 // to control the speed
-input   wire    [11:0]      m3r_pwmLenWant      ;	
-input   wire    [11:0]      m3r_pwmMinMask      ;	
-input   wire    [3:0]       lcStep              ;	
-input   wire    [15:0]      slLen               ;	
+input   wire    [11:0]      m3r_pwmLenWant          ;	
+input   wire    [11:0]      m3r_pwmMinMask          ;	
+input   wire    [3:0]       lcStep                  ;	
+input   wire    [15:0]      slLen                   ;	
 
-output  wire    [15:0]      plLen               ;	
+output  wire    [15:0]      plLen                   ;	
 
 /*
 always @( slStep ) begin
